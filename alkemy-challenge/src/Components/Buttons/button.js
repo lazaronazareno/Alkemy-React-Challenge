41219747f1ec61c790d29a192a137a5a4d147445
lazaronazareno <lazarofmd@DESKTOP-R3OA6UB.localdomain>
2021-05-27 18:11:50 -0300

    fixed some responsive styles

--- alkemy-challenge/src/Components/Buttons/button.js
+++ alkemy-challenge/src/Components/Buttons/button.js
@@ -4,7 +4,7 @@ import './buttonStyles.scss';
 function GeneralButton(props) {
     return (
         <button onClick={props.onClick} id={props.id} className="generalButton">
-            <img className="img" src={props.image} id={props.id}/>
+            <img className="img" src={props.image} id={props.id} alt={props.id}/>
             {props.name}
         </button>
     )
