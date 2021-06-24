738de544e2bf545980632844bfd1bcbb41153dbc
lazaronazareno <lazaronazareno@gmail.com>
2021-06-23 21:01:40 -0300

    styles

--- alkemy-challenge/src/Components/Header/Header.js
+++ alkemy-challenge/src/Components/Header/Header.js
@@ -9,15 +9,15 @@ function Header(props) {
     return (
         <header className="container-fluid d-flex position-absolute top-0 bg-primary border-bottom">
                 <h2 className="me-md-auto mt-3 text-dark text-decoration-none">Welcome to Superheroes Searcher Page</h2>
-                <div className="list-group list-group-horizontal d-flex" role="group" aria-label="Basic outlined example">
-                    <Link className="list-group-item list-group-item-action d-flex text-center text-dark align-items-center list-group-item-warning" to="/search">
+                <div className=" container list-group list-group-horizontal d-flex" role="group" aria-label="Basic outlined example">
+                    <Link className="list-group-item list-group-item-action d-flex text-center text-dark fs-4 align-items-center justify-content-center list-group-item-warning" to="/search">
                         Search 
                     </Link> 
-                    <Link className="list-group-item list-group-item-action d-flex text-center text-dark align-items-center list-group-item-warning" to="/team">
+                    <Link className="list-group-item list-group-item-action d-flex text-center text-dark fs-4 align-items-center justify-content-center list-group-item-warning" to="/team">
                          My Team 
                     </Link> 
                     {props.button.button === true && (
-                        <Link className="list-group-item list-group-item-action d-flex text-center text-dark align-items-center list-group-item-warning"
+                        <Link className="list-group-item list-group-item-action d-flex text-center text-dark fs-4 align-items-center justify-content-center list-group-item-warning"
                          to="/"
                          onClick={onclick}
                         >Logout
